1922e87b35a60f7a6008df87eb91285ff254a511
Rajesh <rajeshghosh4007@gmail.com>
2015-09-09 11:30:04 +0600

    jquery_fixed

---
+++ dust.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rajesh
+ * Date: 9/9/15
+ * Time: 10:50 AM
+ */
+?>
+<!-- <table >
+
+                            <?php
+$sl=0;
+foreach($fetch_result as $value)
+{
+    $sl++;
+
+    ?>
+                            <tr class="student-row" >
+                                <td><?php echo $sl;?></td>
+                                <td>
+                                <p class="student-id" data-id="<?php echo $value['s_id'];?>"><?php echo $value['s_id'];?></p>
+                                </td>
+                                <td>
+                                <p id="js_s_name" class="student-name" data-name="<?php echo $value['name'];?>"><?php echo $value['name'];?></p>
+                                </td>
+                                <td><a class=" add_score btn btn-success" href="#" data-toggle="modal" data-target=".pop"> Add Score</a></td>
+                            </tr>
+                            <?php }?>
+                        </table> -->
