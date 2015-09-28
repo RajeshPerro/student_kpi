9ddb12a8cbfd5d8240507cac3b0c8b9891fea34f
Rajesh <rajeshghosh4007@gmail.com>
2015-09-28 10:57:48 +0600

    attendance_search page added

---
+++ attendance_search.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rajesh
+ * Date: 9/28/15
+ * Time: 10:55 AM
+ */
+$Batch=$_GET['batch'];
+$Group=$_GET['group'];
+$Date=$_GET['date'];
+echo $Batch.$Group.$Date;
+?>
--- js/attendance_edit.js
+++ js/attendance_edit.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
     atten_date.datepicker({dateFormat: "yy-mm-dd"});
 
        $(".atten-parameter").change(function(){
-       console.log("wtf"+"Batch"+batch_id.val()+"Group:"+group_id.val()+"Date:"+atten_date.val());
-    });
+       console.log("wtf"+"  Batch"+batch_id.val()+"Group:"+group_id.val()+"Date:"+atten_date.val());
+           $.ajax({
+
+               type: "GET",
+               url: "attendance_search.php",
+               headers: { 'x-my-custom-header': '' },
+               data: 'batch=' + $('#batch_id').val() + '&group=' + $('#group_id').val() + '&date='+atten_date.val(),
+               success: function(aa){
+                   $('#records_table').html(aa);
+               },
+               error: function(x)
+               {
+
+               }
+
+           });
+       });
 });
