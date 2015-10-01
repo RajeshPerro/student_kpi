670892894e008468b20c7c51f031c6436048978b
Rajesh <rajeshghosh4007@gmail.com>
2015-10-01 14:21:51 +0600

    need to change updatge query

--- score_update_controller.php
+++ score_update_controller.php
@@ -5,5 +5,30 @@
  * Date: 10/1/15
  * Time: 9:56 AM
  */
+include('rajesh_model.php');
+include('database_config.php');
+session_start();
 
+//Registration Page Data Insert
+//echo $_POST['user_name'].$_POST['password'];
+
+if(isset($_POST['entry_date']))//Mandatory field name
+{   $post=$_POST;
+    //$file = $_FILES;
+    $id=$_POST['s_id'];
+    $date=$_POST['entry_date'];
+    $db_user =$database_user;
+    $db_pass =$databse_pass;
+    $table = 'student_assessment';
+    $db_name='student_kpi';
+
+
+    //$raj_modelobject->Data_Update($post,$id,$date,$table,$db_user,$db_pass,$db_name);
+
+    //data_insert_withfile($post,$file,$table,$db_user,$db_pass,$db_name);
+    // data_insert($post,'user',$user,$pass,$dbname);//model's function Name(postdata,file,TableName)
+    echo("<script>alert('Successfully Updated!')</script>");
+    echo("<script>location.href='score_update.php'</script>");
+
+}
 ?>
