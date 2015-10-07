90f33474f816f253b37ecdbbe2332203ba53304a
Rajesh <rajeshghosh4007@gmail.com>
2015-10-07 11:02:55 +0600

    upload added for all table

--- upload_result.php
+++ upload_result.php
@@ -37,7 +37,9 @@ $db_user =$database_user;
 $db_pass =$databse_pass;
 $dbcon=$connection_object->connection('localhost',$db_user,$db_pass,'student_kpi');
 
-if(isset($_POST["submit"]))
+echo $table_name= $_POST['table_type'];
+
+if(isset($_POST["table_type"]) && $table_name ==='assessment')
 {
     $file = $_FILES['file']['tmp_name'];
     $handle = fopen($file, "r");
@@ -75,7 +77,74 @@ if(isset($_POST["submit"]))
     }
 
 }
+elseif(isset($_POST["table_type"]) && $table_name ==='attendance' )
+{
+echo "Yes!";
+    $file = $_FILES['file']['tmp_name'];
+    $handle = fopen($file, "r");
+    $c = 0;
+    while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
+    {
+        $s_id = $filesop[0];
+        $name = $filesop[1];
+        $batch_id = $filesop[2];
+        $group_id = $filesop[3];
+        $attendance = $filesop[4];
+        $entry_date = $filesop[5];
 
+        $sql="INSERT INTO student_attendance (s_id,name,b_id,g_id,attendance,entry_date) VALUES  ('$s_id','$name','$batch_id','$group_id','$attendance','$entry_date')";
+//        print_r($sql);
+//        exit;
+        $preparedStatement = $dbcon->prepare($sql);
+        $preparedStatement->execute();
+        $c = $c + 1;
+    }
+
+    if($sql)
+    {
+        echo("<script>alert('Successfully Uploaded!')</script>");
+        echo("<script>location.href='upload_result.php'</script>");
+    }
+    else
+    {
+        echo("<script>alert('No.No.No')</script>");
+    }
+
+}
+elseif(isset($_POST["table_type"]) && $table_name ==='worksnap' )
+{
+    echo "Yes!";
+    $file = $_FILES['file']['tmp_name'];
+    $handle = fopen($file, "r");
+    $c = 0;
+    while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
+    {
+        $s_id = $filesop[0];
+        $name = $filesop[1];
+        $batch_id = $filesop[2];
+        $group_id = $filesop[3];
+        $hours = $filesop[4];
+        $entry_date = $filesop[5];
+
+        $sql="INSERT INTO worksnap (s_id,name,b_id,g_id,hours,entry_date) VALUES  ('$s_id','$name','$batch_id','$group_id','$hours','$entry_date')";
+//        print_r($sql);
+//        exit;
+        $preparedStatement = $dbcon->prepare($sql);
+        $preparedStatement->execute();
+        $c = $c + 1;
+    }
+
+    if($sql)
+    {
+        echo("<script>alert('Successfully Uploaded!')</script>");
+        echo("<script>location.href='upload_result.php'</script>");
+    }
+    else
+    {
+        echo("<script>alert('No.No.No')</script>");
+    }
+
+}
 ?>
 
 <html>
@@ -119,16 +188,7 @@ if(isset($_POST["submit"]))
 
 
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <!--                    <ul class="nav navbar-nav navbar-left">-->
-                        <!--                        <li><a href="dashboard.php?user=--><?php //echo $_SESSION['user']?><!-- &token=--><?php //echo $_SESSION['token']?><!--">-->
-                        <!--                                <span class="glyphicon glyphicon-dashboard"></span>-->
-                        <!--                                Dashboard</a></li>-->
-                        <!--                        <li><a href="attendance.php"><span class="glyphicon glyphicon-check"></span> Attendance</a></li>-->
-                        <!--                        <li><a href="input_score.php"><span class="glyphicon glyphicon-send"></span> Input Score</a></li>-->
-                        <!--                        <li><a href="logout.php" id="sign-out"><span class="glyphicon glyphicon-log-out"></span> Sign Out</a></li>-->
-                        <!--                        <li><a href="dashboard.php"><span class=""></span>--><?php //echo $_SESSION['user']; ?><!--</a></li>-->
-                        <!---->
-                        <!--                    </ul>-->
+
                         <?php include('header.php'); ?>
                     </div>
 
@@ -152,7 +212,24 @@ if(isset($_POST["submit"]))
                 </ul>
             </div>
             <div class="row">
+
                 <form name="import" method="post" enctype="multipart/form-data">
+                    <div class="col-xs-12 col-sm-12">
+                        <div class="col-xs-12 col-sm-2">
+
+                        </div>
+                        <div class="col-xs-12 col-sm-2">
+                            <p>Select Upload type</p>
+                        </div>
+                        <div class="col-xs-12 col-sm-3">
+                            <select name="table_type" id="table-select">
+                                <option>--Select--</option>
+                                <option value="assessment">Result</option>
+                                <option value="attendance">Attendance</option>
+                                <option value="worksnap">Worksnap</option>
+                            </select>
+                        </div>
+                    </div>
                     <div class="col-md-8 col-sm-4">
 
                         <div class="col-xs-5 col-sm-5 text-right font">Select Your CSV file:</div>
